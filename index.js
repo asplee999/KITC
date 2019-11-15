b04d9fbbb88ab722df81f7349c56666cce9cf4e7
KinoLien <miecowbai@gmail.com>
2019-11-15 11:56:48 +0800

    Add the mysql and mongo insert functions.

--- index.js
+++ index.js
@@ -1,140 +1,5 @@
-// const MongoClient = require('mongodb').MongoClient;
-// const mysql = require('mysql');
-
-// const axios = require('axios');
-
-// const mongoUrl = "mongodb://localhost:27017";		
-// const mongodbName = "sensor";
-// const mongoCollectionName = "rawdata";
-
-// const mysqlConnection = mysql.createConnection({
-// 	host     : 'localhost',
-// 	user     : 'root',
-// 	password : '9583ttld',
-// 	database : 'soon8'
-// });
-// const mysqlTablename = "sensor_rawdata";
-
-// function objectToParameters(obj){
-// 	return Object.keys(obj).map(function(key){ return key + "=" + obj[key]; }).join('&');
-// }
-
-// function lessTenAddZero(v) { return v < 10? ("0" + v) : v; };
-
-// Date.prototype.yymmddHHmmss = function() {
-// 	var mm = this.getMonth() + 1, // getMonth() is zero-based
-//         dd = this.getDate();
-//     return [
-//     	[ this.getFullYear(), lessTenAddZero(mm), lessTenAddZero(dd) ].join('-'),
-//     	[ lessTenAddZero(this.getHours()), lessTenAddZero(this.getMinutes()), lessTenAddZero(this.getSeconds()) ].join(':')
-//     ].join(" ");
-// };
-
-// const houseIeee = "00137A000003AC18";
-// const ieeeList = ["00137A0000047DC4","00137A0000047DE8"];
-// const dataKeys = ["voltage","temperature","humidity"];
-
-// const requestParameters = {
-// 	json: JSON.stringify({houseIeee:houseIeee,orderBy:""}),
-// 	"encodemet%20hod": "AES",
-// 	houseIeeeSecret: houseIeee,
-// 	sign: "AAA"
-// };
-
-// const remoteUrl = "http://210.61.40.166:8081/zigBeeDevice/deviceController/getfindlist.do?";
-
-// const responseRe = new RegExp("null\\((.*)\\)");
-
-// new Promise(function(resolve, reject){
-// 	const fetchedDatetime = (new Date()).yymmddHHmmss();
-// 	axios.get(remoteUrl + objectToParameters(requestParameters))
-// 		.then(function(result){
-// 			const matches = result.data.match(responseRe);
-// 			let dataSet = [];
-// 			if (matches && matches.length) {
-// 				const response = JSON.parse(matches[1]);
-// 				const resList = response.response_params;
-// 				if (resList && resList.length){
-// 					dataSet = resList.filter(function(item){
-// 						return ieeeList.indexOf(item.ieee) != -1;
-// 					})
-// 					.map(function(item){
-// 						let insertData = {
-// 							_last_time: fetchedDatetime,
-// 							_name: item.deviceName,
-// 							_houseieee: item.houseIeee,
-// 							_ieee: item.ieee
-// 						};
-// 						let ids = item.clusterIds.split(',');
-// 						let values = item.vals.split(',').map(function(val){return parseFloat(val).toFixed(2); });
-// 						let valueMap = {};
-						
-// 						ids.forEach(function(id, idx){
-// 							valueMap[id] = values[idx];
-// 						});
-
-// 						var sortedKeys = Object.keys(valueMap);
-
-// 						sortedKeys.sort();
-
-// 						sortedKeys.forEach(function(key, idx){
-// 							insertData[ dataKeys[idx] ] = valueMap[key];
-// 						});
-
-// 						return insertData;
-// 					});
-// 				}
-// 			}
-// 			resolve(dataSet);
-// 		})
-// 		.catch(function(err){ reject(err); });
-// })
-// .then(function(results){
-
-// 	let sqlCommand = "";
-// 	let sqlValues = results.map(function(item){
-// 		return "(" + 
-// 			[
-// 				"'" + item._ieee + "'", 
-// 				"'" + item._last_time + "'", 
-// 				item.voltage, 
-// 				item.temperature, 
-// 				item.humidity
-// 			].join(',')
-// 		+ ")";
-// 	});
-
-// 	if ( sqlValues.length ) {
-// 		sqlCommand = "INSERT INTO " + mysqlTablename + " (ieee, time, voltage, temperature, humidity) VALUES " + sqlValues.join(",");
-// 	}
-
-// 	MongoClient.connect(mongoUrl, function(err, client) {
-// 		if(err) throw err;
-
-// 		const db = client.db(mongodbName);
-
-// 		results.forEach(function(data){
-// 			db.collection(mongoCollectionName,function(err,collection){
-// 				collection.insertOne(data);
-// 			});
-// 			console.log("Inserted: " + JSON.stringify(data) );
-// 		})
-
-// 		client.close();
-// 	});
-
-// 	if (sqlCommand) {
-// 		mysqlConnection.connect();
-
-// 		mysqlConnection.query(sqlCommand, function (error, results, fields) {
-// 			if (error) throw error;
-			
-// 			console.log("Inserted: " + sqlValues.join(",") );
-// 		});
-
-// 		mysqlConnection.end();
-// 	}
-// });
+const MongoClient = require('mongodb').MongoClient;
+const mysql = require('mysql');
 
 // about date time
 const datetimeFormat = "YYYY-MM-DD HH:mm:ss";
@@ -145,10 +10,21 @@ const previousMins = 62;	// to ignore errors if voltage delay
 // secrets
 const secrets = require('./secrets');
 
+// db connect config
+const mysqlConf = secrets.mysql.connect;
+const mongoConf = secrets.mongo;
+
 // devices
 const devices = require('./devices');
 
 const utils = require('./utils');
+
+const mysqlConnection = mysql.createConnection({
+	host     : mysqlConf.host,
+	user     : mysqlConf.user,
+	password : mysqlConf.password,
+	database : mysqlConf.database
+});
 
 const attrMaps = {
 	temperature: "temperature",
@@ -192,7 +68,7 @@ const generateCollects = (ieee, map) => {
 	});
 };
 
-const generateSql = (collects) => {
+const generateSqlValues = (collects) => {
 	let sqlValues = collects.map(tuple => {
 		return "(" + tableCols.map(col => {
 			let name = col.name;
@@ -203,44 +79,83 @@ const generateSql = (collects) => {
 		}).join(",") + ")";
 	});
 	
-	let resCommand = "INSERT INTO " + secrets.mysql.table + " (" + tableCols.map(c => c.name).join(",") + ") VALUES " + sqlValues.join(",");
-
-	return resCommand;
+	return sqlValues;
 };
 
-Object.keys(devices).forEach(async (ieee) => {
-	const devIds = devices[ieee];
+async function mainProcess(){
+	let lstDevs = Object.keys(devices);
+	let totalCollects = [];
+	let totalSqlValues = [];
+	await async function(){
+		let ieee;
+		while ( ieee = lstDevs.pop() ){
+			const devIds = devices[ieee];
 
-	let resdata = await utils.requestData({
-		op: "list_attr",
-		house_ieee: ieee,
-		dev_ids: devIds,
-		// attr: "battery_voltage",
-		start_time: moment(executeDatetime).subtract(previousMins, 'm').format(datetimeFormat),
-		end_time: executeDatetime,
-		pagenum: 1,
-		pagesize: 100
-	});	
+			let resdata = await utils.requestData({
+				op: "list_attr",
+				house_ieee: ieee,
+				dev_ids: devIds,
+				// attr: "battery_voltage",
+				start_time: moment(executeDatetime).subtract(previousMins, 'm').format(datetimeFormat),
+				end_time: executeDatetime,
+				pagenum: 1,
+				pagesize: 100
+			});	
 
-	if (Array.isArray(resdata.result)) {
-		let resMap = getPropMapArrayObject(resdata.result, 'dev_id');
+			if (Array.isArray(resdata.result)) {
+				let resMap = getPropMapArrayObject(resdata.result, 'dev_id');
 
-		Object.keys(resMap).forEach(function(id){
-			let attrRaws = getPropMapArrayObject(resMap[id], 'attr');
-			let attrLast = {};
-			Object.keys(attrRaws).forEach(function(attr){
-				attrLast[attr] = (attrRaws[attr] || []).pop();
-			});
-			resMap[id] = attrLast;
+				Object.keys(resMap).forEach(function(id){
+					let attrRaws = getPropMapArrayObject(resMap[id], 'attr');
+					let attrLast = {};
+					Object.keys(attrRaws).forEach(function(attr){
+						attrLast[attr] = (attrRaws[attr] || []).pop();
+					});
+					resMap[id] = attrLast;
+				});
+
+				let collects = generateCollects(ieee, resMap);
+				let sqlValues = generateSqlValues(collects);
+				
+				if ( collects.length ) totalCollects = totalCollects.concat(collects);
+				if ( sqlValues.length ) totalSqlValues = totalSqlValues.concat(sqlValues);
+				
+			} else console.log(resdata.result);
+
+			await utils.sleep(5000);
+		}
+	}();
+
+	// mysql insert
+	if ( totalSqlValues.length ) {
+		let sqlCommand = "INSERT INTO " + secrets.mysql.table + " (" + tableCols.map(c => c.name).join(",") + ") VALUES " + totalSqlValues.join(",");
+
+		mysqlConnection.connect();
+		mysqlConnection.query(sqlCommand, function (error, results, fields) {
+			if (error) throw error;
+			console.log("Inserted: " + totalSqlValues.join(",") );
 		});
 
-		let collects = generateCollects(ieee, resMap);
-		let sqlCommand = generateSql(collects);
-		
-		console.log(collects);
-		console.log(sqlCommand);
-		
-	} else console.log(resdata);
+		mysqlConnection.end();
+	}
 
-	await utils.sleep(5000);
-});
+	// mongo insert
+	if ( totalCollects.length ) {
+		MongoClient.connect(mongoConf.url, function(err, client) {
+			if(err) throw err;
+
+			const db = client.db(mongoConf.db);
+
+			totalCollects.forEach(function(data){
+				db.collection(mongoConf.collection, function(err, collection){
+					collection.insertOne(data);
+				});
+				console.log("Inserted: " + JSON.stringify(data) );
+			});
+
+			client.close();
+		});
+	}
+}
+
+mainProcess();
